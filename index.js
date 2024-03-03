886f6e530b2f78b37a8b933dbb4e3708e12e824a
Matthew Lowery <matthewdlowery@msn.com>
2024-03-03 13:21:54 -0600

    Started building out function to render shapes and created lib files

---
+++ index.js
@@ -0,0 +1,29 @@
+// Inquirer (Node Package Manager) Import
+const inquirer = require("inquirer");
+
+// File System Module (Node Package Manager) Import
+const fs = require("fs");
+
+// Import Classes from ./lib/shapes Directory
+const { Triangle, Square, Circle } = require("./lib/shapes");
+
+// Function that writes an SVG file based on user input from inquirer prompts
+function writeToFile(fileName, answers) {
+  let svgString = "";
+  svgString =
+  '<svg version="1.1" width="500" height="500" xmlns="http://www.w3.org/2000/svg">'
+  svgString += "<g>";
+  svgString += `${answers.shape}`;
+
+  let shapeChoice;
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}" />`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    svgString += `<rect x="73" y="40" width="250" height="250" fill="${answers.shapeBackgroundColor}" />`;
+  } else {
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}" />`
+  }
+}
